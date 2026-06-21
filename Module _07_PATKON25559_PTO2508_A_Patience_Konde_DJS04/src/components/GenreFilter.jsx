8db795de72260966a5e94f3e09ec88e78f6499fb
patience-konde <kondepatience5@gmail.com>
2026-06-21 19:00:19 +0200

    added style and genrefilter

---
+++ Module _07_PATKON25559_PTO2508_A_Patience_Konde_DJS04/src/components/GenreFilter.jsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { PodcastContext } from "../context/PodcastContext";
+import  styles from "./GeneFilter.module.css"
+
+/**
+ * @param {{genres: {id:number,name:string}[]}} pros - list of genre from data
+ */
+export default function Genrefilter({genre}) {
+    const { genre,setGenre} =usecontext(PodcastContext);
+
+    return (
+        <select className={styles.select}
+        value={genre}
+        onChange={(e) => setGenre(e.target.value)}
+        >
+            <option value="all">all Genres </option>
+            {genres.map((g) => (
+                <Option key={g.id} value={g.id}>
+                    {g.title}
+                </Option>
+            ))}
+
+        </select>
+    )
+}
