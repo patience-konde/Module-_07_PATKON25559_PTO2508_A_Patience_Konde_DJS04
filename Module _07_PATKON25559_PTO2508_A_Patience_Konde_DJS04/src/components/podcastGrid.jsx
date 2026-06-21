2dd7b110f2c322ebfc3748bd0bee551b6dac6439
patience-konde <kondepatience5@gmail.com>
2026-06-21 19:57:32 +0200

    added app styles

---
+++ Module _07_PATKON25559_PTO2508_A_Patience_Konde_DJS04/src/components/podcastGrid.jsx
@@ -0,0 +1,29 @@
+import PodcastCard from "./PodcastCard";
+
+/**
+ * Display a grid layout of podcast preview cards.Each card includes
+ * podcast details: image, title, number of seasons,
+ *  genres(as styled tags), and the last updated date.
+ * @param {Object} props
+ * @param {array<Object>} props.podcasts - Array of podcast  objects to display.
+ * @param {array<Object>} props.genres - Array of genre objects for mapping IDs to titles.
+ * @return {JSX.Element} the rendered podcast grid component.
+ */
+export default function PodcastGrid({ podcasts, genres }) {
+    if (!podcasts?.length) {
+        return <p className={StyleSheet.noResults}>
+            No podcasts match your seach or filter.
+            </p>;
+    }
+
+    return (
+        <>
+         <div className={StyleSheet.grid}>
+            {podcasts.map((podcast) => (
+                <PodcastCard key={podcast.id} podcast={podcast} genres={genres} />
+            ))}
+        </div>
+        </>
+      
+    );
+}
